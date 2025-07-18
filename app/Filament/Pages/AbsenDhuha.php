3c7b03d3c37db757963ff4bb8aa7af33a9b59e72
anang354 <anangegga@gmail.com>
2025-07-18 14:09:25 +0700

    Whatsapp Otomatis pada Halaman Manual Absen Siswa

--- app/Filament/Pages/AbsenDhuha.php
+++ app/Filament/Pages/AbsenDhuha.php
@@ -14,6 +14,7 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Contracts\HasTable;
 use Illuminate\Database\Eloquent\Model;
 use Filament\Notifications\Notification;
+use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Tables\Enums\ActionsPosition;
 use Filament\Tables\Concerns\InteractsWithTable;
@@ -49,12 +50,21 @@ class AbsenDhuha extends Page implements HasTable
         )
         ->columns([
             TextColumn::make('nisn'),
-            TextColumn::make('nama'),
-            TextColumn::make('jenis_kelamin'),
+            TextColumn::make('nama')->searchable(),
+            TextColumn::make('kelas.nama_kelas'),
+            TextColumn::make('jenis_kelamin')->toggleable(isToggledHiddenByDefault: true),
             TextColumn::make('nomor_hp'),
             TextColumn::make('wali'),
         ])
-        ->filters([])
+        ->filters([
+            SelectFilter::make('kelas')
+                    ->relationship('kelas', 'nama_kelas') // Ini adalah kuncinya!
+                    ->label('Filter Berdasarkan Kelas')
+                    ->placeholder('Pilih Kelas')
+                    ->options(
+                        \App\Models\Kelas::pluck('nama_kelas', 'id')->toArray()
+                    ),
+        ])
         ->actions([
             Action::make('Hadir')
             ->color('success')
@@ -75,6 +85,7 @@ class AbsenDhuha extends Page implements HasTable
                             ->body('Absensi ' . $record->nama . ' berhasil dicatat sebagai Hadir.')
                             ->success()
                             ->send();
+                    $this->sendWhatsappWali($record->nama, $record->nomor_hp, 'hadir');
                 } else {
                     Notification::make()
                             ->title('Gagal!')
@@ -110,6 +121,7 @@ class AbsenDhuha extends Page implements HasTable
                             ->body('Absensi ' . $record->nama . ' berhasil dicatat sebagai Izin.')
                             ->success()
                             ->send();
+                    $this->sendWhatsappWali($record->nama, $record->nomor_hp, 'izin');
                 } else {
                     Notification::make()
                             ->title('Gagal!')
@@ -136,6 +148,7 @@ class AbsenDhuha extends Page implements HasTable
                             ->body('Absensi ' . $record->nama . ' berhasil dicatat sebagai Sakit.')
                             ->success()
                             ->send();
+                    $this->sendWhatsappWali($record->nama, $record->nomor_hp, 'sakit');
                 } else {
                     Notification::make()
                             ->title('Gagal!')
@@ -162,6 +175,7 @@ class AbsenDhuha extends Page implements HasTable
                             ->body('Absensi ' . $record->nama . ' berhasil dicatat sebagai Alpha.')
                             ->success()
                             ->send();
+                    $this->sendWhatsappWali($record->nama, $record->nomor_hp, 'alpha');
                 } else {
                     Notification::make()
                             ->title('Gagal!')
@@ -171,5 +185,94 @@ class AbsenDhuha extends Page implements HasTable
                 }
             }),
         ], position: ActionsPosition::BeforeColumns);
+    }
+
+    public function sendWhatsappWali(string $namaSiswa, int $phoneNumber, string $status)
+    {
+        $token = \App\Models\Pengaturan::first()->value('token_whatsapp');
+        $user = \App\Models\User::whereIn('level', ['superadmin', 'admin'])->get();
+
+        if($token === null) { return; }
+
+        $fonnteApiUrl = 'https://api.fonnte.com/send'; // URL ini bisa berubah, cek dokumentasi FonNte terbaru
+        if($status === 'hadir') {
+            $message = "Assalamualaikum Wr.Wb\n";
+            $message .= "Bapak/Ibu wali murid dari " . $namaSiswa . ".\n"; // Baris baru setelah nama siswa
+            $message .= "Putra/putri Anda telah melakukan presensi hari ini:\n"; // Baris baru
+            $message .= "- Tanggal: " . date('l, d M Y') . "\n"; // Baris baru
+            $message .= "- Waktu: " .  date('H:i:s') . "\n"; // Baris baru
+            $message .= "- Sholat: Dhuha \n"; // Baris baru
+            $message .= "\n"; // Baris kosong untuk spasi
+            $message .= "Terima kasih atas perhatiannya.\n";
+            $message .= "\n *_Pesan otomatis tidak perlu dibalas._";
+        } else {
+            $message = "Assalamualaikum Wr.Wb\n";
+            $message .= "Bapak/Ibu wali murid dari " . $namaSiswa . ".\n"; // Baris baru setelah nama siswa
+            $message .= "Putra/putri Anda tidak melakukan presensi hari ini:\n"; // Baris baru
+            $message .= "- Ketarangan: " . $status . "\n"; // Baris baru
+            $message .= "- Sholat: Dhuha  \n"; // Baris baru
+            $message .= "\n"; // Baris kosong untuk spasi
+            $message .= "Terima kasih atas perhatiannya.\n";
+            $message .= "\n *_Pesan otomatis tidak perlu dibalas._";
+        }
+        try {
+            // Inisialisasi sesi cURL
+            $ch = curl_init();
+
+            // Set URL dan opsi cURL
+            curl_setopt($ch, CURLOPT_URL, $fonnteApiUrl);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Mengembalikan transfer sebagai string daripada output langsung
+            curl_setopt($ch, CURLOPT_POST, true); // Mengatur metode request ke POST
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query([ // Membangun query string dari array data
+                'target' => $phoneNumber,
+                'message' => $message,
+            ]));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [ // Menambahkan header Authorization
+                'Authorization: ' . $token,
+            ]);
+
+            // Eksekusi sesi cURL dan dapatkan respons
+            $response = curl_exec($ch);
+
+            // Periksa jika ada error cURL
+            if (curl_errno($ch)) {
+                $error_msg = curl_error($ch);
+                curl_close($ch);
+                Notification::make()
+                    ->title('Kesalahan Pengiriman Whatsapp')
+                    ->danger()
+                    ->body($error_msg)
+                    ->sendToDatabase($user);
+            }
+
+            // Dapatkan informasi HTTP status code
+            $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+            // Tutup sesi cURL
+            curl_close($ch);
+
+            $responseData = json_decode($response, true);
+
+            if ($http_code >= 200 && $http_code < 300) {
+                // Berhasil mengirim pesan (status code 2xx)
+               return;
+                
+            } else {
+                // Gagal mengirim pesan
+                Notification::make()
+                    ->title('Kesalahan Pengiriman Whatsapp')
+                    ->danger()
+                    ->body($responseData)
+                    ->sendToDatabase($user);
+            }
+
+        } catch (\Exception $e) {
+            // Tangani error jika terjadi masalah koneksi atau lainnya
+            Notification::make()
+                    ->title('Kesalahan Pengiriman Whatsapp')
+                    ->danger()
+                    ->body($e->getMessage())
+                    ->sendToDatabase($user);
+        }
     }
 }
